b41af1a040bf9c962d141893a79dc919801270aa
mlboczar <mlboczar@gmail.com>
2017-05-23 15:08:24 -0400

    merge

--- src/main/java/com/grandcircus/spring/controller/FBLogin.java
+++ src/main/java/com/grandcircus/spring/controller/FBLogin.java
@@ -1,8 +1,5 @@
 package com.grandcircus.spring.controller;
 
-/**
- * Created by adamm on 3/14/2017.
- */
 public class FBLogin  {
     static final String FB_APP_ID = "1815068002151981";
     static final String FB_APP_SECRET = "64b3dc1cd7416dd2d9e9188bb7ec4e01";
